38bfcdb4406690251c41fa6099b64054490467d1
Le Thi Cam Tien <thicamtien.le@users-iMac.localdomain>
2019-11-25 22:55:57 -0500

    add layout

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,19 @@
 //This is where we build WORK page
+(function() {
+	console.log("JavaScrip is linked up");
+
+	var button = document.querySelector("#button");
+	var burgerCon = document.querySelector("#burgerCon");
+
+	function hamburgerMenu() {
+		console.log("button clicked");
+		//ad class or toggle to button and burger container
+		burgerCon.classList.toggle("slideToggle");
+		button.classList.toggle("expanded");
+	}
+
+	button.addEventListener("click", hamburgerMenu);
+})();
+
+
+
