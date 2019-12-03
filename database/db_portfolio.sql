0224040f10841c8ced99967c04119e60ab91a642
Le Thi Cam Tien <thicamtien.le@users-iMac.localdomain>
2019-12-02 23:11:08 -0500

    silly effect

--- database/db_portfolio.sql
+++ database/db_portfolio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Nov 29, 2019 at 04:02 AM
+-- Generation Time: Dec 03, 2019 at 03:15 AM
 -- Server version: 5.7.26
 -- PHP Version: 7.3.8
 
@@ -40,8 +40,8 @@ CREATE TABLE `tbl_work` (
 --
 
 INSERT INTO `tbl_work` (`ID`, `Project`, `Role`, `Detail`, `Link`, `background`) VALUES
-(1, 'ThermoSecure', 'Branding', 'This is the 1-day project that I\'ve done with my team. We have to create the entire website, advertisement, and branding for the thermostat (product of Homecom). In this project, I did the logo, brand guideline and many advertisements stuff such as a sell sheet, poster or business card. ', 'https://www.behance.net/gallery/88007993/ThermoSecure', 'thermobg.jpg'),
-(2, 'TRAA website', 'Web designer', 'This is a project that I did with my friend - Jimmy. I played a designer role on this project. I arranged all the infomation on the actual website Anglers.org and redesign the whole website. My main idea about this project is that create a simple website that people easy to follow the information and the news.', 'anglers.org', 'traabg.jpg'),
+(1, 'ThermoSecure by HomeCom', 'Branding', 'This is the 1-day project that I\'ve done with my team. We have to create the entire website, advertisement, and branding for the thermostat (product of Homecom). In this project, I did the logo, brand guideline and many advertisements stuff such as a sell sheet, poster or business card. ', 'https://www.behance.net/gallery/88007993/ThermoSecure', 'thermobg.jpg'),
+(2, 'Thames River Anglers website', 'Web designer', 'This is a project that I did with my friend - Jimmy. I played a designer role on this project. I arranged all the infomation on the actual website Anglers.org and redesign the whole website. My main idea about this project is that create a simple website that people easy to follow the information and the news.', 'anglers.org', 'traabg.jpg'),
 (3, 'Couple Hotel Website', 'Web Designer', 'I re-designed the Couple Hotel website without changing logo or any information on the main website. My main idea about this design is that I want to provide the users the most important things when they searching for a hotel such as type of room, prices and the services.', 'https://www.behance.net/gallery/87975617/Couples-Resort', 'couplebg.jpg'),
 (4, 'The Organ REGEN Project', 'Graphic designer', 'In this project, I made the logo, brochure, and poster to help people know more about the Organ Regeneration Project which is run by OTTLab.', '', 'organbg.jpg'),
 (5, 'Many bullshit videos I made', '3D, Video Editor', 'I\'d practiced a lot with After Effect, Premiere and Cinema 4D. It\'s fun when I working with video and 3D stuff. You can check out some of my videos on Youtube.', 'youtube.com', 'videobg.jpg');
