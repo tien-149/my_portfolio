f4fdfeb2ce6d3f0c0b78ae0cf3bada961d30e61b
Le Thi Cam Tien <thicamtien.le@users-iMac.localdomain>
2019-12-09 13:18:38 -0500

    fix problem

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const sql = require('../utils/sql');
+// const sql = require('../utils/sql');
 const connect = require('../utils/sqlConnect');
 
 
