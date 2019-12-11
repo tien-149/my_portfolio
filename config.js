88f5bc57e1d7ca5248c54d20da0372c11a2b497b
Le Thi Cam Tien <thicamtien.le@users-iMac.localdomain>
2019-12-11 13:02:27 -0500

    another try

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
-    host: "localhcleardb.netst",
+    host: "us-cdbr-iron-east-05.cleardb.net",
     // port: 8889, // 8889 for mac
     uname: "b6f6967263d264",
     pword: "3169b54a", // leave blank for windows
     database: "heroku_c600e604ec49fdf"
-}
+};
 
 module.exports = config;
